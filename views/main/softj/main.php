ee1a7b1e3062c1bcbcd3bb960cd0ed52ae03822a
SUN <sunjoo0418@gmail.com>
2022-07-04 17:26:47 +0900

    채용공고버튼 추가

--- views/main/softj/main.php
+++ views/main/softj/main.php
@@ -10,7 +10,6 @@
                     <br/><b>통합 솔루션</b>을 제공합니다.
                 </p>
                 <a class="abtn mg10" type="button" onclick="window.open('https://softj.net/jobopening')"><b>웹 개발자</b> 채용공고 보기</a>
-                <a class="abtn" type="button" onclick="window.open('https://softj.net/jobopening/sub/job.html')">채용중인 공고 보기</a>
             </div>
         </div>
         <div class="temp-right">
